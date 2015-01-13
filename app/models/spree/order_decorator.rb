471c19ac6ca2186da90e52de7d9cf72c2cf74090
Luke Duncalfe <lduncalfe@eml.cc>
2015-01-14 10:28:00 +1300

    Adding logging to overridden Order method.

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -2,6 +2,7 @@ Spree::Order.class_eval do
 
   # Override this method to do nothing, as no payments will need to be processed
   def process_payments!
+    Rails.logger.info('process_payments! has been overridden by spree_pxpay_paymentmethod gem to do nothing')
   end
 
 end
